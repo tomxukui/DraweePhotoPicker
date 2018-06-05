4fd7f34e5b88597e01a41b6c0438c623ab9c5aa4
tom <152780382@qq.com>
2018-06-05 15:11:51 +0800

    1.0.0

--- app/src/main/java/com/ablingbling/app/draweephotopicker/MainActivity.java
+++ app/src/main/java/com/ablingbling/app/draweephotopicker/MainActivity.java
@@ -3,14 +3,13 @@ package com.ablingbling.app.draweephotopicker;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.OrientationHelper;
-import android.support.v7.widget.RecyclerView;
-import android.support.v7.widget.StaggeredGridLayoutManager;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.ablingbling.app.draweephotopicker.permission.DefaultRationale;
 import com.ablingbling.app.draweephotopicker.permission.PermissionSetting;
+import com.ablingbling.library.ninegridlayout.CreateNineGridLayout;
 import com.yanzhenjie.permission.Action;
 import com.yanzhenjie.permission.AndPermission;
 import com.yanzhenjie.permission.Permission;
@@ -24,12 +23,13 @@ import com.ablingbling.library.draweephotopicker.PhotoPreview;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static final int EXTRA_PICK = 1;
-    private static final int EXTRA_PREVIEW = 2;
+    private static final int SPAN_COUNT = 3;
+    private static final int MAX_COUNT = 9;
+    private static final int REQUEST_PICK_IMG = 1;
+    private static final int REQUEST_PREVIEW_IMG = 2;
 
-    private PhotoAdapter photoAdapter;
+    private CreateDraweeNineGridLayout grid_create;
 
-    private ArrayList<String> selectedPhotos = new ArrayList<>();
     private Rationale mRationale;
     private PermissionSetting mPermissionSetting;
 
@@ -38,12 +38,137 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        initData();
+        initView();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode) {
+
+            case REQUEST_PICK_IMG: {
+                if (resultCode == RESULT_OK) {
+                    List<String> imgPaths = data.getStringArrayListExtra(PhotoPicker.EXTRA_SELECTED_PHOTOS);
+                    grid_create.setNewData(imgPaths);
+                }
+            }
+            break;
+
+            case REQUEST_PREVIEW_IMG: {
+                if (resultCode == RESULT_OK) {
+                    List<String> imgPaths = data.getStringArrayListExtra(PhotoPreview.EXTRA_PHOTO_PATHS);
+                    grid_create.setNewData(imgPaths);
+                }
+            }
+            break;
+
+            default:
+                break;
+
+        }
+    }
+
+    private void initData() {
         mRationale = new DefaultRationale();
         mPermissionSetting = new PermissionSetting(this);
+    }
 
+    private void initView() {
+        grid_create = findViewById(R.id.grid_create);
+
+        grid_create.setMaxCount(MAX_COUNT);
+        grid_create.setMaxColumn(SPAN_COUNT);
+        grid_create.setOnAddClickListener(new CreateNineGridLayout.OnAddClickListener() {
+
+            @Override
+            public void onAddClick(CreateNineGridLayout createNineGridLayout, ImageView imageView) {
+                pickImgsWithPermission();
+            }
+
+        });
+        grid_create.setOnItemClickListener(new CreateNineGridLayout.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(CreateNineGridLayout createNineGridLayout, View view, int i, String s) {
+                previewImgs(i);
+            }
+
+        });
+        grid_create.notifyDataSetChanged();
+
+        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                PhotoPicker.builder()
+                        .setPhotoCount(9)
+                        .setGridColumnCount(3)
+                        .start(MainActivity.this, REQUEST_PICK_IMG);
+            }
+
+        });
+
+        findViewById(R.id.button_no_camera).setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                PhotoPicker.builder()
+                        .setPhotoCount(7)
+                        .setShowCamera(false)
+                        .setPreviewEnabled(false)
+                        .start(MainActivity.this, REQUEST_PICK_IMG);
+            }
+
+        });
+
+        findViewById(R.id.button_one_photo).setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                PhotoPicker.builder()
+                        .setPhotoCount(1)
+                        .start(MainActivity.this, REQUEST_PICK_IMG);
+            }
+
+        });
+
+        findViewById(R.id.button_photo_gif).setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                PhotoPicker.builder()
+                        .setShowCamera(true)
+                        .setShowGif(true)
+                        .start(MainActivity.this, REQUEST_PICK_IMG);
+            }
+
+        });
+    }
+
+    private void pickImgs() {
+        PhotoPicker.builder()
+                .setPhotoCount(MAX_COUNT)
+                .setShowGif(false)
+                .setPreviewEnabled(true)
+                .setGridColumnCount(4)
+                .setShowCamera(true)
+                .setSelected((ArrayList<String>) grid_create.getImages())
+                .start(this, REQUEST_PICK_IMG);
+    }
+
+    private void pickImgsWithPermission() {
         AndPermission.with(this)
                 .permission(Permission.Group.STORAGE, Permission.Group.CAMERA)
                 .rationale(mRationale)
+                .onGranted(new Action() {
+
+                    @Override
+                    public void onAction(List<String> permissions) {
+                        pickImgs();
+                    }
+
+                })
                 .onDenied(new Action() {
 
                     @Override
@@ -57,117 +182,14 @@ public class MainActivity extends AppCompatActivity {
 
                 })
                 .start();
-
-        RecyclerView recyclerView = findViewById(R.id.recycler_view);
-        photoAdapter = new PhotoAdapter(this, selectedPhotos);
-
-        recyclerView.setLayoutManager(new StaggeredGridLayoutManager(4, OrientationHelper.VERTICAL));
-        recyclerView.setAdapter(photoAdapter);
-
-        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View v) {
-                PhotoPicker.builder()
-                        .setPhotoCount(9)
-                        .setGridColumnCount(3)
-                        .start(MainActivity.this, EXTRA_PICK);
-            }
-
-        });
-
-        findViewById(R.id.button_no_camera).setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View v) {
-                PhotoPicker.builder()
-                        .setPhotoCount(7)
-                        .setShowCamera(false)
-                        .setPreviewEnabled(false)
-                        .start(MainActivity.this, EXTRA_PICK);
-            }
-
-        });
-
-        findViewById(R.id.button_one_photo).setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View v) {
-                PhotoPicker.builder()
-                        .setPhotoCount(1)
-                        .start(MainActivity.this, EXTRA_PICK);
-            }
-
-        });
-
-        findViewById(R.id.button_photo_gif).setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View v) {
-                PhotoPicker.builder()
-                        .setShowCamera(true)
-                        .setShowGif(true)
-                        .start(MainActivity.this, EXTRA_PICK);
-            }
-
-        });
-
-        recyclerView.addOnItemTouchListener(new RecyclerItemClickListener(this, new RecyclerItemClickListener.OnItemClickListener() {
-
-            @Override
-            public void onItemClick(View view, int position) {
-                if (photoAdapter.getItemViewType(position) == PhotoAdapter.TYPE_ADD) {
-                    PhotoPicker.builder()
-                            .setPhotoCount(PhotoAdapter.MAX)
-                            .setShowCamera(true)
-                            .setPreviewEnabled(false)
-                            .setSelected(selectedPhotos)
-                            .start(MainActivity.this, EXTRA_PICK);
-
-                } else {
-                    PhotoPreview.builder()
-                            .setPhotos(selectedPhotos)
-                            .setCurrentItem(position)
-                            .start(MainActivity.this, EXTRA_PREVIEW);
-                }
-            }
-
-        }));
     }
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        switch (requestCode) {
-
-            case EXTRA_PICK: {
-                if (resultCode == RESULT_OK) {
-                    List<String> photos = data.getStringArrayListExtra(PhotoPicker.EXTRA_SELECTED_PHOTOS);
-                    selectedPhotos.clear();
-                    if (photos != null) {
-                        selectedPhotos.addAll(photos);
-                    }
-                    photoAdapter.notifyDataSetChanged();
-                }
-            }
-            break;
-
-            case EXTRA_PREVIEW: {
-                if (resultCode == RESULT_OK) {
-                    List<String> photos = data.getStringArrayListExtra(PhotoPreview.EXTRA_PHOTO_PATHS);
-                    selectedPhotos.clear();
-                    if (photos != null) {
-                        selectedPhotos.addAll(photos);
-                    }
-                    photoAdapter.notifyDataSetChanged();
-                }
-            }
-            break;
-
-            default:
-                break;
-
-        }
+    private void previewImgs(int position) {
+        PhotoPreview.builder()
+                .setPhotos((ArrayList<String>) grid_create.getImages())
+                .setCurrentItem(position)
+                .setShowDeleteButton(true)
+                .start(this, REQUEST_PREVIEW_IMG);
     }
 
 }
